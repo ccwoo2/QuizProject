6b0b08697bd10e214e39dc77b22500c2004f270c
Peter Chon <125619428+ccwoo2@users.noreply.github.com>
2024-04-04 15:07:19 -0400

    frontend

---
+++ App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './homepage';
+import Dashboard from './dashboard';
+import Play from './play';
+import CreateQuestion from './create';
+import {BrowserRouter, Routes, Route} from "react-router-dom" 
+import Register from './register';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      {/* <header className="App-header">
+
+      </header> */}
+      
+        <Routes>
+          <Route path='/' element={<Homepage/>}/> 
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/play' element={<Play/>}/>
+          <Route path='/create' element={<CreateQuestion/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
